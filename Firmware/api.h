1a73b5f3a9688fd2dcc2d891c6bdacbe0bf3c6e9
Dr.zzm32 <drzzm32@gmail.com>
2016-11-23 17:44:32 +0800

    Create api.h
    add api definitions

---
+++ Firmware/api.h
@@ -0,0 +1,51 @@
+#ifndef uint8_t
+#define uint8_t unsigned char
+#endif
+#ifndef uint16_t
+#define uint16_t unsigned short
+#endif
+#ifndef uint32_t
+#define uint32_t unsigned int
+#endif
+
+virtual void setup();
+virtual void loop();
+
+uint32_t _millis, _micros;
+uint32_t millis();
+uint32_t micros();
+void delay(uint32_t time);
+void delayMicroseconds(uint32_t time);
+
+void initScreen();
+void openScreen();
+void closeScreen();
+void setBrgiht(char value);
+
+uint8_t _backColor, _foreColor, _fontSize;
+void backColor(uint8_t color);
+void foreColor(uint8_t color);
+void drawPixel(char x, char y, uint8_t color);
+void drawLine(char x1, char y1, char x2, char y2);
+void drawTri(char x1, char y1, char x2, char y2, char x3, char y3, uint8_t fillFlag);
+void drawRect(char x1, char y1, char x2, char y2, uint8_t fillFlag);
+void drawBitmap(char x, char y, char w, char h, uint8_t* data);
+void drawColoredBitmap(char x, char y, char w, char h, uint8_t transColor, uint8_t* data);
+void gBegin();  //CS` = 0;
+void gEnd();    //CS` = 1;
+
+void fontSize(char size);
+void drawChar(char x, char y, char c);
+void print(char x, char y, char* format, ...)
+
+#define LPAD_UP     0x01
+#define LPAD_DOWN   0x02
+#define LPAD_LEFT   0x04
+#define LPAD_RIGHT  0x08
+#define RPAD_UP     0x10
+#define RPAD_DOWN   0x20
+#define RPAD_LEFT   0x40
+#define RPAD_RIGHT  0x80
+
+uint8_t checkKeys();
+uint8_t waitKeyUp(char key);
