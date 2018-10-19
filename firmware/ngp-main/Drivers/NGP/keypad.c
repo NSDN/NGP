5924822a38f501909c6eb34bfaae50e6aa296b01
drzzm32 <drzzm32@gmail.com>
2018-10-19 10:47:05 +0800

    keypad ok

--- firmware/ngp-main/Drivers/NGP/keypad.c
+++ firmware/ngp-main/Drivers/NGP/keypad.c
@@ -7,17 +7,17 @@ uint8_t scanKeys() {
 
 	HAL_GPIO_WritePin(KEY_OUTA_GPIO_Port, KEY_OUTA_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(KEY_OUTB_GPIO_Port, KEY_OUTB_Pin, GPIO_PIN_SET);
-	if (HAL_GPIO_ReadPin(KEY_INA_GPIO_Port, KEY_INA_Pin) == GPIO_PIN_RESET) value |= LPAD_DOWN;
-	if (HAL_GPIO_ReadPin(KEY_INB_GPIO_Port, KEY_INB_Pin) == GPIO_PIN_RESET) value |= LPAD_LEFT;
-	if (HAL_GPIO_ReadPin(KEY_INC_GPIO_Port, KEY_INC_Pin) == GPIO_PIN_RESET) value |= LPAD_UP;
+	if (HAL_GPIO_ReadPin(KEY_INA_GPIO_Port, KEY_INA_Pin) == GPIO_PIN_RESET) value |= LPAD_UP;
+	if (HAL_GPIO_ReadPin(KEY_INB_GPIO_Port, KEY_INB_Pin) == GPIO_PIN_RESET) value |= LPAD_DOWN;
+	if (HAL_GPIO_ReadPin(KEY_INC_GPIO_Port, KEY_INC_Pin) == GPIO_PIN_RESET) value |= LPAD_LEFT;
 	if (HAL_GPIO_ReadPin(KEY_IND_GPIO_Port, KEY_IND_Pin) == GPIO_PIN_RESET) value |= LPAD_RIGHT;
 
 	HAL_GPIO_WritePin(KEY_OUTB_GPIO_Port, KEY_OUTB_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(KEY_OUTA_GPIO_Port, KEY_OUTA_Pin, GPIO_PIN_SET);
-	if (HAL_GPIO_ReadPin(KEY_INA_GPIO_Port, KEY_INA_Pin) == GPIO_PIN_RESET) value |= RPAD_DOWN;
-	if (HAL_GPIO_ReadPin(KEY_INB_GPIO_Port, KEY_INB_Pin) == GPIO_PIN_RESET) value |= RPAD_RIGHT;
-	if (HAL_GPIO_ReadPin(KEY_INC_GPIO_Port, KEY_INC_Pin) == GPIO_PIN_RESET) value |= RPAD_UP;
-	if (HAL_GPIO_ReadPin(KEY_IND_GPIO_Port, KEY_IND_Pin) == GPIO_PIN_RESET) value |= RPAD_LEFT;
+	if (HAL_GPIO_ReadPin(KEY_INA_GPIO_Port, KEY_INA_Pin) == GPIO_PIN_RESET) value |= RPAD_UP;
+	if (HAL_GPIO_ReadPin(KEY_INB_GPIO_Port, KEY_INB_Pin) == GPIO_PIN_RESET) value |= RPAD_DOWN;
+	if (HAL_GPIO_ReadPin(KEY_INC_GPIO_Port, KEY_INC_Pin) == GPIO_PIN_RESET) value |= RPAD_LEFT;
+	if (HAL_GPIO_ReadPin(KEY_IND_GPIO_Port, KEY_IND_Pin) == GPIO_PIN_RESET) value |= RPAD_RIGHT;
 
 	return value;
 }
